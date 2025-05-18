883f72181f1d17e30ac09bd580ee31db70272f6f
Nii Larteh Annan <annanniilarteh@gmail.com>
2025-05-18 18:18:21 +0000

    ready for Ci

---
+++ calc/target/debug/deps/calc-a4cbab327b9a822d.d
@@ -0,0 +1,9 @@
+/workspaces/Rust-template/calc/target/debug/deps/libcalc-a4cbab327b9a822d.rmeta: src/main.rs Cargo.toml
+
+/workspaces/Rust-template/calc/target/debug/deps/calc-a4cbab327b9a822d.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ calc/target/debug/deps/calc-e6d645b9732f906d.d
@@ -0,0 +1,9 @@
+/workspaces/Rust-template/calc/target/debug/deps/libcalc-e6d645b9732f906d.rmeta: src/lib.rs Cargo.toml
+
+/workspaces/Rust-template/calc/target/debug/deps/calc-e6d645b9732f906d.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
